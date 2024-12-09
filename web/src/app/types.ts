493581b75800ebc270dce946042f4ef7c8296b9e
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2024-12-09 12:36:27 +0900

    修正

--- web/src/app/types.ts
+++ web/src/app/types.ts
@@ -13,7 +13,7 @@ declare module "next-auth" {
       email?: string;
       image?: string;
       accessToken?: string | unknown;
-      login?: boolean;
+      login?: string | unknown;
     };
   }
 }
